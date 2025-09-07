2a70b2b6ebed8ec0dba7f28ba477a3ce270894fa
ERSUCC <isaacbrown5763@gmail.com>
2025-09-07 15:33:24 -0500

    Use more robust method of checking for headless

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name         := "language-library"
 organization := "org.nlogo.languagelibrary"
-version      := "3.3.2"
+version      := "3.3.3"
 isSnapshot   := true
 
 scalaVersion          := "3.7.0"
@@ -13,7 +13,7 @@ def cclArtifacts(path: String): String =
   s"https://s3.amazonaws.com/ccl-artifacts/$path"
 
 libraryDependencies ++= Seq(
-  "org.nlogo"          % "netlogo"        % "7.0.0-RC1-e8801f2"
+  "org.nlogo"          % "netlogo"        % "7.0.0-2486d1e"
 , "org.json4s"        %% "json4s-jackson" % "4.0.7"
 // not used by this library, but needed for NetLogo
 , "org.jogamp.jogl" % "jogl-all" % "2.4.0" from cclArtifacts("jogl-all-2.4.0.jar")
--- src/org/nlogo/langextension/StreamGobbler.scala
+++ src/org/nlogo/langextension/StreamGobbler.scala
@@ -4,7 +4,6 @@ import java.io.{ BufferedReader, InputStream, InputStreamReader }
 import javax.swing.SwingUtilities
 
 import org.nlogo.api.{ OutputDestination, Workspace }
-import org.nlogo.languagelibrary.config.Platform
 
 // Adapted from this StackOverflow answer: https://stackoverflow.com/a/1732506
 // -Jeremy B July 2023
@@ -20,7 +19,7 @@ class StreamGobbler(ws: Workspace, is: InputStream) extends Thread {
 
   private def output(s: String): Unit = {
     Logger.current.logMany("Subprocess.output()") { Seq("s", s) }
-    if (ws.isHeadless || Platform.isHeadless) {
+    if (!ws.workspaceContext.workspaceGUI) {
       println(s)
     } else {
       SwingUtilities.invokeLater { () =>
